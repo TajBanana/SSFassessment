0849ba4848e5104778b20fc0ce9a08bb8eea1955
TajBanana <ytahjian@gmail.com>
2022-01-18 16:01:00 +0800

    added redis config

--- src/main/java/tajbanana/ssfassessment/Constants.java
+++ src/main/java/tajbanana/ssfassessment/Constants.java
@@ -6,5 +6,9 @@ public class Constants {
     public static final String OPEN_LIBRARY_URL_WORKS = "https://openlibrary.org/works/";
 
     public static final String BEAN_BOOK_SERVICE = "book_service";
+    public static final String BEAN_BOOK_CACHE = "book_cache";
+
+    public static final String ENV_REDIS_PASSWORD = "ENV_REDIS_PASSWORD";
+
 
 }
--- src/main/java/tajbanana/ssfassessment/repository/BookRepository.java
+++ src/main/java/tajbanana/ssfassessment/repository/BookRepository.java
@@ -1,4 +1,18 @@
 package tajbanana.ssfassessment.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import static tajbanana.ssfassessment.Constants.*;
+
+@Repository
 public class BookRepository {
+
+    @Autowired
+    @Qualifier(BEAN_BOOK_CACHE)
+
+    private RedisTemplate<String, String> template;
+
 }
